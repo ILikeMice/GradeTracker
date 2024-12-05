77ed43aa64ed4556ef0cf44fd1499f04d59eec66
ILikeMice <ilikemice47@gmail.com>
2024-12-05 17:25:18 +0000

    Started work on Subject adding and editing

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
+data = {}
+chartdata = {
+  type: 'line',
+  data: {
+    
+  },
+  options: {
+      plugins: {
+          legend: {
+              labels: {
+                  font: {
+                      size: 14,
+                      family: "monospace"
+                  }
+              }
+          }
+      },
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+}
+
+chartdata.data.labels = ['Exam 1', 'Exam 2', 'Exam 3', 'Exam 4', 'Exam 5', 'Exam 6']
+chartdata.data.datasets = [{
+  label: 'Subject 1',
+  data: [78,95, 90, 85, 70, 65],
+  borderWidth: 1,
+}]
+
+chartdata.data.datasets.push({
+  label: 'Subject 2',
+  data: [90, 80, 75, 60],
+  borderWidth: 1,
+})
+
 const ctx = document.getElementById('myChart');
 
 
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['Red', 'bazingaaa', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: 'Subject 1',
-        data: [78,95, 90, 85, 70, 65],
-        borderWidth: 1,
-      },{
-        label: 'Subject 2',
-        data: [85, 90, 80, 75, 60, 55],
-        borderWidth: 1,
-      }]
-    },
-    options: {
-        plugins: {
-            legend: {
-                labels: {
-                    font: {
-                        size: 14,
-                        family: "monospace"
-                    }
-                }
-            }
-        },
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+new Chart(ctx, chartdata);
 
   
